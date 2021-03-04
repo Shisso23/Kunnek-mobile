3799e584cf2cfada6eb1f89608eca88ffc7655a3
Brendon <brendon@codehesion.co.za>
2021-03-04 15:13:39 +0200

    User registration

---
+++ src/screens/auth/initial/initial.screen.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Button } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
+import { useTheme } from '../../../theme';
+
+const InitialScreen = () => {
+  const navigation = useNavigation();
+  const { Gutters } = useTheme();
+  return (
+    <View style={[Gutters.smallHMargin]}>
+      <Button mode="contained" onPress={() => navigation.navigate('SignIn')}>
+        Login
+      </Button>
+      <Button mode="outlined" onPress={() => navigation.navigate('Register')}>
+        Create An Account
+      </Button>
+      <Button mode="contained" onPress={() => navigation.navigate('ForgotPassword')}>
+        Forgot Password
+      </Button>
+    </View>
+  );
+};
+
+export default InitialScreen;
