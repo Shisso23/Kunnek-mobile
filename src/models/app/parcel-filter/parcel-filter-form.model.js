1e78fac51d95c19c9b127c7b31162de14e9e48a8
hyacinthe <hyacinthe@codehesion.co.za>
2021-06-09 15:18:10 +0200

    added api model for filtering parcels

--- src/models/app/parcel-filter/parcel-filter-form.model.js
+++ src/models/app/parcel-filter/parcel-filter-form.model.js
@@ -1,10 +1,15 @@
 import _ from 'lodash';
 
 export const filtersFormModel = (filterModal = {}) => ({
-  startLocation: _.get(filterModal, 'startLocation', 0.0),
-  endLocation: _.get(filterModal, 'endLocation', 0.0),
+  startLocation: _.get(filterModal, 'startLocation', ''),
+  endLocation: _.get(filterModal, 'endLocation', ''),
   maximumDistance: _.get(filterModal, 'maximumDistance', 0.0),
-  lastDeliveryDate: _.get(filterModal, 'lastDeliveryDate', new Date()),
+  lastDeliveryDate: _.get(filterModal, 'lastDeliveryDate', ''),
 });
 
-export const filtersFormModelApi = (filterModal = {}) => ({ filterModal });
+export const filtersFormModelApi = (filterModal = {}) => ({
+  start_point: _.get(filterModal, 'startLocation', ''),
+  end_point: _.get(filterModal, 'endLocation', ''),
+  max_distance_deviation: _.get(filterModal, 'maximumDistance', 0),
+  latest_delivery_date: _.get(filterModal, 'lastDeliveryDate', ''),
+});
