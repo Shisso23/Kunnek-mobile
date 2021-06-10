7455a099a7aaf36bdfc0191290cb4fb4c76a171e
hyacinthe <hyacinthe@codehesion.co.za>
2021-06-10 08:37:06 +0200

    added parcel filter service

--- src/models/app/parcel-filter/parcel-filter-form.model.js
+++ src/models/app/parcel-filter/parcel-filter-form.model.js
@@ -3,13 +3,21 @@ import _ from 'lodash';
 export const filtersFormModel = (filterModal = {}) => ({
   startLocation: _.get(filterModal, 'startLocation', ''),
   endLocation: _.get(filterModal, 'endLocation', ''),
-  maximumDistance: _.get(filterModal, 'maximumDistance', 0.0),
+  maximumDistance: _.get(filterModal, 'maximumDistance', 0),
   lastDeliveryDate: _.get(filterModal, 'lastDeliveryDate', ''),
 });
 
-export const filtersFormModelApi = (filterModal = {}) => ({
-  start_point: _.get(filterModal, 'startLocation', ''),
-  end_point: _.get(filterModal, 'endLocation', ''),
-  max_distance_deviation: _.get(filterModal, 'maximumDistance', 0),
-  latest_delivery_date: _.get(filterModal, 'lastDeliveryDate', ''),
-});
+export const filtersFormModelApi = (filterModal = {}) => {
+  const empyFields = _.filter(Object.keys(filterModal), (key) => {
+    return `${filterModal[`${key}`]}` === '0' || `${filterModal[`${key}`]}`.length === 0;
+  });
+  return _.unset(
+    {
+      start_point: _.get(filterModal, 'startLocation', ''),
+      end_point: _.get(filterModal, 'endLocation', ''),
+      max_distance_deviation: _.get(filterModal, 'maximumDistance', 0),
+      latest_delivery_date: _.get(filterModal, 'lastDeliveryDate', ''),
+    },
+    empyFields,
+  );
+};
---
+++ src/services/sub-services/parcel-filter/parcel-filter.service.js
@@ -0,0 +1,26 @@
+import _ from 'lodash';
+
+import { filtersFormModelApi } from '../../../models/app/parcel-filter/parcel-filter-form.model';
+import authNetworkService from '../auth-network-service/auth-network.service';
+import parcelRequestsUrls from '../../sub-services/parcel-request-service/parcel-request.urls';
+import { parcelRequestModel } from '../../../models/app/parcel-request/parcel-request.model';
+
+const filterParcels = async ({ params }) => {
+  const url = parcelRequestsUrls.parcelRequestsUrl(params);
+  const apiModal = filtersFormModelApi(params);
+  const nonEmptyParams = _.reduce(
+    apiModal,
+    (parameters = '?', value, field) => (parameters += `${field}=${value}&`),
+  );
+
+  const _createAndReturnListModel = (apiResponse) =>
+    _.map(_.get(apiResponse, 'data.data', []), (item) => parcelRequestModel(item));
+  const apiResponse = await authNetworkService.get(
+    `${url}${nonEmptyParams.substring(0, nonEmptyParams.length - 1)}`,
+  );
+  return _createAndReturnListModel(apiResponse);
+};
+
+export default {
+  filterParcels,
+};
