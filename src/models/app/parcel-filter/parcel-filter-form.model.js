c3296de64f6fbc4de17d4de0402fbc050cfdc973
hyacinthe <hyacinthe@codehesion.co.za>
2021-06-11 14:55:19 +0200

    fixed request fields bug

--- src/models/app/parcel-filter/parcel-filter-form.model.js
+++ src/models/app/parcel-filter/parcel-filter-form.model.js
@@ -3,25 +3,28 @@ import _ from 'lodash';
 export const filtersFormModel = (filterModal = {}) => ({
   startLocation: _.get(filterModal, 'startLocation', ''),
   endLocation: _.get(filterModal, 'endLocation', ''),
-  maximumDistance: _.get(filterModal, 'maximumDistance', 0),
+  maximumDistance: _.get(filterModal, 'maximumDistance', 50),
   lastDeliveryDate: _.get(filterModal, 'lastDeliveryDate', ''),
 });
 
 export const apiFiltersFormModel = (filterModal = {}) => {
-  const nonEmptyFields = _.filter(Object.keys(filterModal), (key) => {
-    return `${filterModal[`${key}`]}`.length > 0 && `${filterModal[`${key}`]}` !== '0';
-  });
-
   let apiModel = {
     start_point: _.get(filterModal, 'startLocation', ''),
     end_point: _.get(filterModal, 'endLocation', ''),
-    max_distance_deviation: _.get(filterModal, 'maximumDistance', 0),
+    max_distance_deviation:
+      _.get(filterModal, 'maximumDistance', 0) === 0
+        ? 50
+        : _.get(filterModal, 'maximumDistance', 0),
     latest_delivery_date: _.get(filterModal, 'lastDeliveryDate', ''),
   };
+  const nonEmptyFields = _.filter(Object.keys(apiModel), (key) => {
+    return `${apiModel[`${key}`]}`.length > 0 && `${apiModel[`${key}`]}` !== '0';
+  });
+
   apiModel = _.reduce(
     nonEmptyFields,
     (finalObject, field) => {
-      finalObject[`${field}`] = `${filterModal[field]}`;
+      finalObject[`${field}`] = `${apiModel[field]}`;
       return finalObject;
     },
     {},
