3b4d32acaf09950bfcfd0d30fae3e1907a53b087
Zandré Broodryk <zandre.broodryk@codehesion.co.za>
2021-07-12 11:58:31 +0200

    added the T's&C's url and linked to them in account creation.

--- src/components/atoms/terms-and-conditions/index.jsx
+++ src/components/atoms/terms-and-conditions/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity, Linking } from 'react-native';
 import { CheckBox, Text } from 'react-native-elements';
 import useTheme from '../../../theme/hooks/useTheme';
+import config from '../../../config';
 
 const TermsAndConditions = ({ checked, onPress }) => {
   const { Common, Colors, Layout } = useTheme();
+
+  const _openTermsAndConditions = () => {
+    Linking.openURL(config.termsAndConditionsUrl);
+  };
+
   return (
     <View style={[Layout.row, Layout.alignItemsCenter]}>
       <CheckBox
@@ -16,7 +22,7 @@ const TermsAndConditions = ({ checked, onPress }) => {
         checkedColor={Colors.primary}
       />
       <Text>I agree to the</Text>
-      <TouchableOpacity delayPressIn={0} onPress={() => null}>
+      <TouchableOpacity delayPressIn={0} onPress={_openTermsAndConditions}>
         <Text style={[Common.link]}> Terms and Conditions</Text>
       </TouchableOpacity>
     </View>
