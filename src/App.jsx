92ce19ebb83b3df68dc55f85086927b670d301a5
Zandré Broodryk <zandre.broodryk@codehesion.co.za>
2021-07-16 11:21:16 +0200

    initial android setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,45 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import NavigationContainer from './navigation/root.navigator';
 import { initAppAction } from './reducers/app-reducer/app.actions';
+import messaging from '@react-native-firebase/messaging';
+import { Alert } from 'react-native';
 
 const App = () => {
   const dispatch = useDispatch();
 
+  messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+    console.warn('Message handled in the background!', remoteMessage);
+  });
+
   useEffect(() => {
     dispatch(initAppAction());
+    requestUserPermission();
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+    return unsubscribe;
   }, []);
+
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      getFcmToken();
+      console.warn('Authorization status:', authStatus);
+    }
+  };
+
+  const getFcmToken = async () => {
+    const fcmToken = await messaging().getToken();
+    if (fcmToken) {
+      console.warn('Your Firebase Token is:', fcmToken);
+    } else {
+      console.warn('Failed', 'No token received');
+    }
+  };
 
   return <NavigationContainer />;
 };
